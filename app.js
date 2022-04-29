ec5db3f51b526e2ca04ae19d511355402094ff37
Deepak Krishnan E <deepak.krishnan.e@gmail.com>
2022-04-30 01:04:13 +0530

    feat: search form activation complete

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 let navbar = document.querySelector(".navbar");
 let cartItem = document.querySelector(".cart-items-container");
-
+let searchForm = document.querySelector(".search-form")
 
 document.querySelector("#menu-btn").onclick = () => {
     navbar.classList.toggle('active');
+}
+
+document.querySelector("#search-btn").onclick = () => {
+    searchForm.classList.toggle('active')
 }
 
 document.querySelector("#cart-btn").onclick = () => {
